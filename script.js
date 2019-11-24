f17c5600fbdcd01ca3331bb0016b4a26fce026f3
Stan <stanley.k.anderson@gmail.com>
2019-11-23 19:01:44 -0500

    initial checkin

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+Get the element from the HTML document with the id "demo" and sets its contents to be "Hello World!"
+*/
+
+function changeText(){
+    document.getElementById("gpId").innerHTML = "Hello World";
+}
+
+function goToLink(){
+    window.location="Dinosaurgame.com"
+}
